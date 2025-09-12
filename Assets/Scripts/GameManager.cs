aa4e1bec90339d1e5dc8644993c60ff0b865dc46
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-09-12 10:01:48 +0700

    scoring for player and computer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+    public Text playerScoreText;
+    public Text computerScoreText;
+    private int playerScore;
+    private int computerScore;
+
+    public void PlayerScores()
+    {
+        playerScore++;
+        this.playerScoreText.text = playerScore.ToString();
+        ResetRound();
+    }
+
+    public void ComputerScores()
+    {
+        computerScore++;
+        this.computerScoreText.text = computerScore.ToString();
+        ResetRound();
+    }
+
+    private void ResetRound()
+    {
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -10,6 +10,10 @@ public class Paddle : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-
+    public void ResetPosition()
+    {
+        rb.position = new Vector2(rb.position.x, 0f);
+        rb.linearVelocity = Vector2.zero;
+    }
 }
 
